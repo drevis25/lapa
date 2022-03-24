b2e3484e6f7f275bd8b32cc8778c68c0642592fb
drevis25 <98223240+drevis25@users.noreply.github.com>
2022-03-24 14:17:22 +0200

    yo

---
+++ db.php
@@ -0,0 +1,3 @@
+<?php
+$conn = new mysqli("sql304.epizy.com", "epiz_31343774", "JsbMXTwMmc", "epiz_31343774_pasaulesgids");
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasaules Gids</title>
+</head>
+<body>
+<form action="cfg.php" method="post">
+<div class=login>
+<h1>Account login</h1>
+<p>username</p>
+<input type='text' />
+<p>password</p>
+<input type='password' />
+<button>login</button>
+</div>
+</body>
+</html>
